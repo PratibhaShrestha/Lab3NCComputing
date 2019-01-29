47c572a66d86c145f255668c41e3466c690aeb55
Unknown <karla_yvett@hotmail.com>
2019-01-29 15:10:04 +0100

    får ikke datoen fram

--- src/EchoUcaseServerTCP.java
+++ src/EchoUcaseServerTCP.java
@@ -3,6 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class EchoUcaseServerTCP {
     public static void main(String[] args) throws IOException {
@@ -37,14 +46,59 @@ public class EchoUcaseServerTCP {
                         new InputStreamReader(connectSocket.getInputStream()));
         ) {
             InetAddress clientAddr = connectSocket.getInetAddress();
+
             int clientPort = connectSocket.getPort();
+
             String receivedText;
+
             // read from the connection socket
             while ((receivedText = in.readLine()) != null) {
                 System.out.println("Client [" + clientAddr.getHostAddress() + ":" + clientPort + "] > " + receivedText);
 
-                String url="http://www.google.com/search?q=";
-                String charset="UTF-8";
+                final String USER_AGENT =
+                        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2";
+
+                String url="http://www.google.com/search";
+                String searchUrl = url + "?q=" + receivedText + "get time" ;
+
+                Connection connection = Jsoup.connect(searchUrl).userAgent(USER_AGENT);
+
+                Document document = connection.get();
+
+                String title = document.title();
+
+            //    System.out.println(title);
+
+                Elements result = document.select("h3.r > a");
+
+                String linkText = result.text();
+
+                System.out.println(linkText);
+
+
+                //Trenger ikke?
+              /*  for (Element results : result) {
+                    String linkHref = result.attr("href");
+                    String linkText = result.text();
+
+                    System.out.println(linkText);
+
+                }
+*/
+
+/*
+//Gjør ingenting??
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)) ;
+
+                String inputLine;
+                while ((inputLine = bufferedReader.readLine()) != null){
+                    System.out.println(inputLine);
+                }
+                bufferedReader.close();
+*/
+
+
+          /*      String charset="UTF-8";
                 String query = String.format("%s+time",URLEncoder.encode(receivedText, charset));
                 URLConnection con = new URL(url+ query).openConnection();
                 System.out.println(query);
@@ -57,11 +111,11 @@ public class EchoUcaseServerTCP {
 
 
                 buffIn.close();
-
-                String outText = "will find you the time, Sir !";
+*/
+             //   String outText = "will find you the time, Sir !";
                 // Write the converted uppercase string to the connection socket
-                out.println(outText);
-                System.out.println("I (Server) [" + connectSocket.getLocalAddress().getHostAddress() + ":" + portNumber + "] > " + outText);
+             //   out.println(outText);
+               System.out.println("I (Server) [" + connectSocket.getLocalAddress().getHostAddress() + ":" + portNumber + "] > ");
             }
             System.out.println("I am done, Bye!");
         } catch (IOException e) {
